10d68cd3834d3f5754301d1f97e00963956eaf77
DianRP43 <55683288+DianRP43@users.noreply.github.com>
2019-09-24 15:25:11 +0700

    Add files via upload
    PBO1 = IF-01

---
+++ PBO1-10118043-Latihan13-Relasi/src/pbo1/pkg10118043/latihan13/relasi/PBO110118043Latihan13Relasi.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo1.pkg10118043.latihan13.relasi;
+
+/**
+ *
+ * @author user
+ */
+public class PBO110118043Latihan13Relasi {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int a1 = 10;
+        int b1 = 20;
+        System.out.println("a == b " + (a1 == b1));    
+        System.out.println("a != b " + (a1 != b1));
+        System.out.println("a > b " + (a1 > b1));
+        System.out.println("a < b " + (a1 < b1));
+        System.out.println("a >= b " + (a1 >= b1));
+        System.out.println("a <= b " + (a1 <= b1));
+    }
+    
+}
